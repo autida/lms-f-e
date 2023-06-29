8ac3704d74ac6e1292b08ffb30fd8d6c853b6f47
autida <autidaanalita@gmail.com>
2023-06-30 04:38:43 +0800

    created a login design

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <header className="App-header">
+            <h2>Welcome to the <br />Assignment Review App</h2>
+        </header>
+    )
+}
+
+export default Header
